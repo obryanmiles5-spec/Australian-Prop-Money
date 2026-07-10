527201900d06ab2bcc14926d1c39678347661272
obryanmiles5-spec <obryanmiles5@gmail.com>
2026-07-10 12:46:28 +0100

    !

--- test-next-image.js
+++
@@ -1,2 +0,0 @@
-const { getImageUrl } = require('./lib/imagekit');
-// Wait, I can't just run that because of TypeScript
--- update_denom.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('app/page.tsx', 'utf8');
-
-code = code.replace(/'denom-card-100\.png'/g, "'images/products/Australian Notes/New-Notes/100 AUD/100-aud-front'");
-code = code.replace(/'denom-card-50\.png'/g, "'images/products/Australian Notes/New-Notes/50 AUD/50-aud-front'");
-code = code.replace(/'denom-card-20\.png'/g, "'images/products/Australian Notes/New-Notes/20 AUD/20-aud-front'");
-
-fs.writeFileSync('app/page.tsx', code);
